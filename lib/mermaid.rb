bffe6e383d609097424592e9de0eed63f74e0f02
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-06-18 11:13:48 -0400

    Writting test for name

--- lib/mermaid.rb
+++ lib/mermaid.rb
@@ -12,14 +12,14 @@ describe Mermaid do
     expect(mermaid.name).to eq 'Sereia'
   end
   
-  it 'has an age' do
-    mermaid = Mermaid.new('Sereia')
-    expect(mermaid.age).to eq 22
-  end
+#   it 'has an age' do
+#     mermaid = Mermaid.new('Sereia')
+#     expect(mermaid.age).to eq 22
+#   end
   
-  it 'has can get older' do
-    mermaid = Mermaid.new('Sereia')
-    mermaid.get_older
-    expect(mermaid.age).to eq 23
-  end
+#   it 'has can get older' do
+#     mermaid = Mermaid.new('Sereia')
+#     mermaid.get_older
+#     expect(mermaid.age).to eq 23
+#   end
 end
